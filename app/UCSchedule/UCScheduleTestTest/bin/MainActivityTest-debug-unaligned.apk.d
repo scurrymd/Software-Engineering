4b6bfbe5f23e4a153ba93401ff390d8caf031b59
Matthew Scurry <scurrymd@mail.uc.edu>
2013-08-01 10:04:09 -0400

    fixed screen rotation bug

--- app/UCSchedule/UCScheduleTestTest/bin/MainActivityTest-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-C:\Users\Arthur - User\Documents\GitHub\Software-Engineering\app\UCSchedule\UCScheduleTestTest\bin\MainActivityTest-debug-unaligned.apk : \
-C:\Users\Arthur - User\Documents\GitHub\Software-Engineering\app\UCSchedule\UCScheduleTestTest\bin\MainActivityTest.ap_ \
-C:\Users\Arthur - User\Documents\GitHub\Software-Engineering\app\UCSchedule\UCScheduleTestTest\bin\classes.dex \
